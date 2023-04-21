c1ade3f1186e40f6e17bd8a4c5b02ced78c54a41
Roy543 <sahilsas88@gmail.com>
2023-04-21 14:18:47 +0200

    First Component Implementation.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            friends:[
+    data() {
+        return {
+            friends: [
                 {
                     id: 'sahil',
                     name: 'Sahil',
@@ -14,15 +14,38 @@ const app = Vue.createApp({
                     phone: '0987654321',
                     email: 'vivek@localhost.com'
                 }
-            ],
-            detailsAreVisible: false,
+            ]
         }
     },
-    methods:{
-        toggleDetails(){
+});
+
+app.component('friend-contact', {
+    template: `
+    <li>
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+          </ul>
+        </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'sahil',
+                name: 'Sahil',
+                phone: '1234567890',
+                email: 'sahil@localhost.com'
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
         }
     }
-});
+})
 
 app.mount('#app');
